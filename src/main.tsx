e78ea94db485daba9a048f13a9947817350cf3fd
Ares <aresnemeia@gmail.com>
2025-11-13 10:33:29 -0300

    Removido Sentry e adicionado Eruda

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://880b004d8a7ada040a20868bd24238b4@o4510357885616128.ingest.us.sentry.io/4510357897936896",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true
-});
 
 createRoot(document.getElementById("root")!).render(<App />);
